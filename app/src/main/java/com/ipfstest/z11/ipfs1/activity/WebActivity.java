b1bd3c60b4a2f17b09defe202d1cf0a8b7bea8a5
zpehome <zpehome@yeah.net>
2019-10-29 16:24:20 +0800

    update WebActivity

--- app/src/main/java/com/ipfstest/z11/ipfs1/activity/WebActivity.java
+++ app/src/main/java/com/ipfstest/z11/ipfs1/activity/WebActivity.java
@@ -2,15 +2,88 @@ package com.ipfstest.z11.ipfs1.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 import com.ipfstest.z11.ipfs1.R;
 
 public class WebActivity extends AppCompatActivity {
 
+    ProgressBar progressBar;
+    WebView webView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web);
+
+        progressBar = findViewById(R.id.pb_loading);
+        webView = findViewById(R.id.m_browser);
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                progressBar.setVisibility(View.GONE);
+                progressBar.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                progressBar.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+
+
+        });
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                super.onProgressChanged(view, newProgress);
+                progressBar.setProgress(newProgress);
+            }
+
+
+        });
+
+        String hash = getIntent().getStringExtra("hash");
+        getSupportActionBar().setTitle("127.0.0.1:8080/ipfs/");
+        getSupportActionBar().setSubtitle(hash);
+        webView.loadUrl("http://127.0.0.1:8080/ipfs/" + hash);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK && webView.canGoBack()) {
+            webView.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (webView != null) {
+            webView.loadDataWithBaseURL(null, "", "text/html", "utf-8", null);
+            webView.clearHistory();
+            ((ViewGroup) webView.getParent()).removeView(webView);
+            webView.destroy();
+            webView = null;
+        }
+        super.onDestroy();
     }
 }
